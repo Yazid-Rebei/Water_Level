7838223cccaf5b0ebbb149e241ee0dd9bca4ffb0
yazid rebei <yazidrebei2000@gmail.com>
2026-03-23 14:32:59 +0100

    Refactor water level monitoring code

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,100 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-#define braudrate 115200
 #define Trig 5
-#define echo 8
-#define Empty_led 24
-#define plein_led 25
-#define meduim_led 26
+#define echo 16
+
+#define Empty_led 25
+#define plein_led 26
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-const float hauteur_boite = 30; // en cm  
+const float hauteur_boite = 14.0;
 
-char* alert(float level){
-  if(level <=5){
-    digitalWrite(Empty_led,LOW);
+const char* alert(float level){
+  if(level >= 10.0){  
     digitalWrite(plein_led,HIGH);
-    digitalWrite(meduim_led,LOW);
-    return "Plein !!";
+    digitalWrite(Empty_led,LOW);
+    return "Plein";
   }
-  else if (level >= 25)
-  {
+  else if(level <= 0.5){
     digitalWrite(Empty_led,HIGH);
     digitalWrite(plein_led,LOW);
-    digitalWrite(meduim_led,LOW);
     return "Vide";
-
   }
-  else{
+  else if(level >=4.5 && level <= 5.5){
     digitalWrite(Empty_led,LOW);
     digitalWrite(plein_led,LOW);
-    digitalWrite(meduim_led,LOW);
+    return "Moyen";
+  }
+  else{
     return " ";
-  }  
+  }
 }
 
 void trigger(){
   digitalWrite(Trig,LOW);
-  delayMicroseconds(5);
+  delayMicroseconds(2);
   digitalWrite(Trig,HIGH);
   delayMicroseconds(10);
   digitalWrite(Trig,LOW);
-  
 }
+
+
 long read_time(){
-  long time=pulseIn(echo,HIGH,30000);
-  return time;
+  return pulseIn(echo, HIGH, 30000);
 }
 
 
-float calc_distacne(){
+float calc_distance(){
   trigger();
   long time = read_time();
-  if(time ==0 ){
+
+  if(time == 0){
     return -1;
   }
-  return (time/2) * 0.0343;
+
+  return (time * 0.0343) / 2.0;
 }
+
+
 float calc_level(float distance){
   return hauteur_boite - distance;
 }
 
+
 void waterLevel(){
-  float distance = calc_distacne();
-  if(distance <=-1){
+
+  float distance = calc_distance();
+  if(distance < 0){
     lcd.setCursor(0,0);
-    lcd.print("ERREUR CAPTEUR !");
+    lcd.print("ERREUR CAPTEUR ");
     return;
   }
 
   float level = calc_level(distance);
-  char* etat = alert(level);
+  const char* etat = alert(level);
+
   lcd.setCursor(0,0);
   lcd.print("Level:");
-  lcd.print(level);
-  lcd.print("cm  ");
+  lcd.print((int)level);
+  lcd.print("cm   ");
+
   lcd.setCursor(0,1);
-  lcd.print("Etat: ");
+  lcd.print("Etat:");
   lcd.print(etat);
   lcd.print("   ");
 }
 
-
-
 void setup(){
   pinMode(echo,INPUT);
   pinMode(Trig,OUTPUT);
+
   pinMode(Empty_led,OUTPUT);
   pinMode(plein_led,OUTPUT);
-  pinMode(meduim_led,OUTPUT);
   lcd.init();
   lcd.backlight();
-  Serial.begin(braudrate);
+  lcd.print("Begin");
+  Serial.begin(9600);
 }
 
 void loop(){
